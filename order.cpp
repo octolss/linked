e1305b9b25ef4e50cee28015c594f2658f1cbd53
octolss <147706639+octolss@users.noreply.github.com>
2024-04-12 10:00:14 +0000

    .

--- order.cpp
+++ order.cpp
@@ -1,39 +1,63 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <cstdlib>
 
 using namespace std;
 
 
 class Cart{
     public:
+
         vector<string> all_items = {"cucumber", "tomato", "milk", "bread", "water", "candy"};
 };
 
-class Person{
+class Person {
     public:
-        string item = all_items[rand() % all_items.size()];
+
+        string item;
+        Person(const vector<string>& items) {
+            if (!items.empty()) {
+                item = items[rand() % items.size()];
+            }
+        }
 };
 
 class Cash{
-    bool opened;
+    
 
     public:
+    
+        int people;
+        int minute = 1;
+        bool opened;
+        float chance = 1.0;
 
         bool ifOpened()
         {
             return opened;
         }
 
-        int people;
-        int minute = 1;
+        float checker(int people, bool opened)
+        {   
+            if(opened == true){
+                if(people > 6)
+                {
+                    chance = 0.05;
+                }
+            } 
+        }
+
+        
 
 };
 
 
 int main(){
 
-
+    Cash firstCash;
+    Cash secondCash;
+    Cash thirdCash;
 
 }
 
